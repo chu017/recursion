8c8536353194b9de27162ee68e86a2272eae3b81
calvin <calvinchenhupro@gmail.com>
2021-02-28 21:01:42 -0800

    Assignment complete

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -7,4 +7,28 @@
 var getElementsByClassName = function(className
 ) {
   // your code here
+
+  // Declare<body>of HTML. and result array
+  var bodyHtml = document.body;
+  // console.log(bodyHtml);
+  var result = [];
+
+  // Declare a function for recursion
+  var hasClass = function(bodyHtml) {
+    if (bodyHtml.classList && bodyHtml.classList.contains(className)) {
+      result.push(bodyHtml);
+    }
+
+    if (bodyHtml.hasChildNodes()) {
+      for (var i = 0; i < bodyHtml.childNodes.length; i++) {
+        // console.log(bodyHtml.childNodes[i]);
+        hasClass(bodyHtml.childNodes[i]);
+      }
+    }
+  };
+
+  hasClass(bodyHtml);
+  // console.log(result);
+  return result;
+
 };
