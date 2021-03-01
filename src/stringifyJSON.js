6dec8e723bb3a1ade34c66664fd7fad92a2288ee
calvin <calvinchenhupro@gmail.com>
2021-02-28 21:03:52 -0800

    Assignment complete

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,56 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+
+  var arrOfKeyValues = [];
+  var arrValues = [];
+  var objKeys = [];
+
+  // base case
+  if (typeof obj === 'number' || typeof obj === 'boolean' || obj === null) {
+    return '' + obj;
+
+  } else if (typeof obj === 'string') {
+    return '"' + obj + '"';
+
+
+  // recursive case - array
+  } else if (Array.isArray(obj)) {
+    if (obj[0] === undefined) {
+      return '[]';
+    } else {
+      obj.forEach(function(item) {
+        arrValues.push(stringifyJSON(item));
+      });
+      return '[' + arrValues + ']';
+    }
+
+  // recursive case - object
+  } else if (obj instanceof Object) {
+    objKeys = Object.keys(obj);
+
+    objKeys.forEach(function(key) {
+      var keyOut = '"' + key + '":';
+      var keyValueOut = obj[key];
+      if (typeof keyValueOut === 'function' ||
+      typeof keyValueOut === undefined) {
+        arrOfKeyValues.push('');
+      } else if (typeof keyValueOut === 'string') {
+        arrOfKeyValues.push(keyOut + '"' + keyValueOut + '"');
+
+      } else if (typeof keyValueOut === 'boolean'
+      || typeof keyValueOut === 'number' || keyValueOut === null) {
+        arrOfKeyValues.push(keyOut + keyValueOut);
+
+        console.log(arrOfKeyValues);
+
+      } else if (keyValueOut instanceof Object) {
+        arrOfKeyValues.push(keyOut + stringifyJSON(keyValueOut));
+      }
+
+    });
+
+    return '{' + arrOfKeyValues + '}';
+  }
+
 };
